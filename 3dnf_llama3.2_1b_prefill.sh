81227cf0a6ca4f9d8f6049fdc687210636cc2027
BUPTslb <shenlibo@bupt.cn>
2025-04-14 19:07:20 +0800

    add 4-14

---
+++ 3dnf_llama3.2_1b_prefill.sh
@@ -0,0 +1,339 @@
+# ./build/bin/Simulator --trace_file p8-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1-2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p8-1.txt
+
+
+# ./build/bin/Simulator --trace_file p16-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-p16-1.txt
+
+# ./build/bin/Simulator --trace_file p32-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-p32-1.txt
+
+# ./build/bin/Simulator --trace_file p64-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-p64-1.txt
+
+./build/bin/Simulator --trace_file p16-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p16-1.txt
+
+./build/bin/Simulator --trace_file p32-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p32-1.txt
+
+./build/bin/Simulator --trace_file p64-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p64-1.txt
+
+./build/bin/Simulator --trace_file p128-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p128-1.txt
+
+# ./build/bin/Simulator --trace_file p256-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1-2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p256-1.txt
+
+# ./build/bin/Simulator --trace_file p512-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-p512-1.txt
+
+./build/bin/Simulator --trace_file p512-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p512-1.txt
+
+# ./build/bin/Simulator --trace_file p1024-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1-2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p1024-1.txt
+
+./build/bin/Simulator --trace_file p2048-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p2048-1.txt
+
+./build/bin/Simulator --trace_file p4096-1.csv --mode \
+    language --config \
+    ./configs/3DNF-channel1-2.json --models_list \
+    ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-2-llama3.2-1b-p4096-1.txt
+
+# ./build/bin/Simulator --trace_file p8-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel2-llama3.2-1b-p8-1.txt
+
+# ./build/bin/Simulator --trace_file p16-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel2-llama3.2-1b-p16-1.txt
+
+# ./build/bin/Simulator --trace_file p32-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel2-llama3.2-1b-p32-1.txt
+
+# ./build/bin/Simulator --trace_file p64-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel2-llama3.2-1b-p64-1.txt
+
+# ./build/bin/Simulator --trace_file p128-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel2.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel2-llama3.2-1b-p128-1.txt
+
+# ./build/bin/Simulator --trace_file p8-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel4.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel4-llama3.2-1b-p8-1.txt
+
+# ./build/bin/Simulator --trace_file p16-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel4.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel4-llama3.2-1b-p16-1.txt
+
+# ./build/bin/Simulator --trace_file p32-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel4.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel4-llama3.2-1b-p32-1.txt
+
+# ./build/bin/Simulator --trace_file p64-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel4.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel4-llama3.2-1b-p64-1.txt
+
+# ./build/bin/Simulator --trace_file p128-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel4.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel4-llama3.2-1b-p128-1.txt
+
+# ./build/bin/Simulator --trace_file p8-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel8.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel8-llama3.2-1b-p8-1.txt
+
+# ./build/bin/Simulator --trace_file p16-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel8.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel8-llama3.2-1b-p16-1.txt
+
+# ./build/bin/Simulator --trace_file p32-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel8.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel8-llama3.2-1b-p32-1.txt
+
+# ./build/bin/Simulator --trace_file p64-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel8.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel8-llama3.2-1b-p64-1.txt
+
+# ./build/bin/Simulator --trace_file p128-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel8.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel8-llama3.2-1b-p128-1.txt
+
+# ./build/bin/Simulator --trace_file p8-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel16.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel16-llama3.2-1b-p8-1.txt
+
+# ./build/bin/Simulator --trace_file p16-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel16.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel16-llama3.2-1b-p16-1.txt
+
+# ./build/bin/Simulator --trace_file p32-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel16.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel16-llama3.2-1b-p32-1.txt
+
+# ./build/bin/Simulator --trace_file p64-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel16.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel16-llama3.2-1b-p64-1.txt
+
+# ./build/bin/Simulator --trace_file p128-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel16.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel16-llama3.2-1b-p128-1.txt
+
+# ./build/bin/Simulator --trace_file p8-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel32.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel32-llama3.2-1b-p8-1.txt
+
+# ./build/bin/Simulator --trace_file p16-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel32.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel32-llama3.2-1b-p16-1.txt
+
+# ./build/bin/Simulator --trace_file p32-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel32.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel32-llama3.2-1b-p32-1.txt
+
+# ./build/bin/Simulator --trace_file p64-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel32.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel32-llama3.2-1b-p64-1.txt
+
+# ./build/bin/Simulator --trace_file p128-1.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel32.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel32-llama3.2-1b-p128-1.txt
+
+# ./build/bin/Simulator --trace_file t16-128.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t16-128.txt
+
+# ./build/bin/Simulator --trace_file t32-16.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t32-16.txt
+
+# ./build/bin/Simulator --trace_file t32-32.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t32-32.txt
+
+# ./build/bin/Simulator --trace_file t32-64.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t32-64.txt
+
+# ./build/bin/Simulator --trace_file t32-128.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t32-128.txt
+
+# ./build/bin/Simulator --trace_file t64-16.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t64-16.txt
+
+# ./build/bin/Simulator --trace_file t64-32.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t64-32.txt
+
+# ./build/bin/Simulator --trace_file t64-64.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t64-64.txt
+
+# ./build/bin/Simulator --trace_file t64-128.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t64-128.txt
+
+# ./build/bin/Simulator --trace_file t128-16.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-16.txt
+
+
+# ./build/bin/Simulator --trace_file t128-32.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-32.txt
+
+
+# ./build/bin/Simulator --trace_file t128-64.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-64.txt
+
+
+
+# ./build/bin/Simulator --trace_file t128-128.csv --mode \
+#     language --config \
+#     ./configs/gemmini.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-128.txt
+
+# ./build/bin/Simulator --trace_file t128-256.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-256.txt
+
+# ./build/bin/Simulator --trace_file t128-512.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-512.txt
+
+# ./build/bin/Simulator --trace_file t128-1024.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t128-1024.txt
+
+# ./build/bin/Simulator --trace_file t256-128.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t256-128.txt
+
+# ./build/bin/Simulator --trace_file t256-256.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t256-256.txt
+
+# ./build/bin/Simulator --trace_file t256-512.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t256-512.txt
+
+# ./build/bin/Simulator --trace_file t256-1024.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t256-1024.txt
+
+# ./build/bin/Simulator --trace_file t512-128.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t512-128.txt
+
+# ./build/bin/Simulator --trace_file t512-256.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t512-256.txt
+
+# ./build/bin/Simulator --trace_file t512-512.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t512-512.txt
+
+# ./build/bin/Simulator --trace_file t512-1024.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t512-1024.txt
+
+# ./build/bin/Simulator --trace_file t1024-128.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t1024-128.txt
+
+# ./build/bin/Simulator --trace_file t1024-256.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t1024-256.txt
+
+# ./build/bin/Simulator --trace_file t1024-512.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t1024-512.txt
+
+# ./build/bin/Simulator --trace_file t1024-1024.csv --mode \
+#     language --config \
+#     ./configs/3DNF-channel1.json --models_list \
+#     ./example/llama3.2-1b.json > results/llama3.2-1b/3DNF-channel1-llama3.2-1b-t1024-1024.txt
---
+++ gemminirun_1.5b.sh
@@ -0,0 +1,79 @@
+./build/bin/Simulator --trace_file t128-128.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t128-128.txt
+
+./build/bin/Simulator --trace_file t128-256.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t128-256.txt
+
+./build/bin/Simulator --trace_file t128-512.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t128-512.txt
+
+./build/bin/Simulator --trace_file t128-1024.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t128-1024.txt
+
+./build/bin/Simulator --trace_file t256-128.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t256-128.txt
+
+./build/bin/Simulator --trace_file t256-256.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t256-256.txt
+
+./build/bin/Simulator --trace_file t256-512.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t256-512.txt
+
+./build/bin/Simulator --trace_file t256-1024.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t256-1024.txt
+
+./build/bin/Simulator --trace_file t512-128.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t512-128.txt
+
+./build/bin/Simulator --trace_file t512-256.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t512-256.txt
+
+./build/bin/Simulator --trace_file t512-512.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t512-512.txt
+
+./build/bin/Simulator --trace_file t512-1024.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t512-1024.txt
+
+./build/bin/Simulator --trace_file t1024-128.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t1024-128.txt
+
+./build/bin/Simulator --trace_file t1024-256.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t1024-256.txt
+
+./build/bin/Simulator --trace_file t1024-512.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t1024-512.txt
+
+./build/bin/Simulator --trace_file t1024-1024.csv --mode \
+    language --config \
+    ./configs/gemmini.json --models_list \
+    ./example/qwen2.5-1.5b.json > results/qwen1.5/gemmini-qwen1.5-t1024-1024.txt
